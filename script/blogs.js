f0c0b23f68c0c6faf87cefd03a5d04f33c7d556d
Tahmid Hossain <tahmidhossain@tahmids-mbp-2.mynetworksettings.com>
2025-03-05 08:34:10 -0500

    fix the path

--- script/blogs.js
+++ script/blogs.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const backBtn = document.getElementById("btn-index");
+
+  if (backBtn) {
+    backBtn.addEventListener("click", function () {
+      window.location.href = "index.html"; // ✅ Redirect to index.html
+    });
+  }
+
+  // Blogs page navigation
   const blogs = document.getElementById("blogsID");
 
   if (blogs) {
