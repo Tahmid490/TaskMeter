5e79d21bec45f35cf000dfe3de181b295dd11c5d
Tahmid Hossain <tahmidhossain@tahmids-mbp-2.mynetworksettings.com>
2025-03-05 07:56:43 -0500

    change the path

--- script/back.js
+++ script/back.js
@@ -1,4 +1,8 @@
-const btnBack = document.getElementById("btn-back");
-btnBack.addEventListener("click", function () {
-  window.location.href = "./index.html";
+// document.getElementById("btn-back").addEventListener("click", function () {
+//   window.location.href = "https://tahmid490.github.io/TaskMeter/index.html";
+// });
+
+const blogs = document.getElementById("blogs");
+blogs.addEventListener("click", function () {
+  window.location.href = "./blogs.html";
 });
--- script/blogs.js
+++
@@ -1,5 +0,0 @@
-const blogs = document.getElementById("blogs");
-
-blogs.addEventListener("click", function () {
-  window.location.href = "https://tahmid490.github.io/TaskMeter/index.html";
-});
